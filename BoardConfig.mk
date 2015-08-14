5f1bd14a0032282ed43569c4a1cdd1386d7eec99
phong 9h <tranthephong33@gmail.com>
2015-08-14 15:16:30 +0700

    First Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,106 @@
+# inherit from the proprietary version
+-include vendor/wiko/rainbow/BoardConfigVendor.mk
+
+# Board
+TARGET_BOARD_PLATFORM := mt6582
+TARGET_NO_BOOTLOADER := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a7
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a7
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE
+COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
+
+BOARD_GPS_LIBRARIES := true
+
+# Partitions
+# make_ext4fs requires numbers in dec format
+BOARD_BOOTIMAGE_PARTITION_SIZE := 6291456
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8629568
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 891289600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_USERIMAGES_USE_EXT4:=true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# Flags
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+TARGET_KMODULES := true
+
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+TARGET_PREBUILT_KERNEL := device/wiko/rainbow/kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/wiko/rainbow/bootimg.mk
+BOARD_MKBOOTIMG_ARGS := --board 1336460062
+#TARGET_PREBUILT_RECOVERY_KERNEL :=
+
+TARGET_RECOVERY_FSTAB := device/wiko/rainbow/rootdir/root/twrp.fstab
+
+# Deodex
+WITH_DEXPREOPT := false
+DISABLE_DEXPREOPT := true
+
+#Camera
+USE_CAMERA_STUB := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_MTK := true
+BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/wiko/rainbow/bluetooth
+
+# EGL settings
+BOARD_EGL_CFG := device/wiko/rainbow/rootdir/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# SELINUX
+BOARD_SEPOLICY_DIRS := \
+       device/wiko/rainbow/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+       device.te \
+       app.te \
+       system.te \
+       file_contexts
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/wiko/rainbow/ril/
+
+BOARD_CONNECTIVITY_VENDOR := MediaTek
+BOARD_CONNECTIVITY_MODULE := conn_soc
+
+# WIFI
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM:="/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA:=STA
+WIFI_DRIVER_FW_PATH_AP:=AP
+WIFI_DRIVER_FW_PATH_P2P:=P2P
